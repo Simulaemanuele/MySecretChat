b322169e6924d1948953e32fbc4a141672fd9a09
Emanuele Simula <simulaemanuele@gmail.com>
2023-11-29 12:43:06 +0100

    feat: created Chat component that renders GiftedChat and Created a Navigation Stack system in App.js

--- App.tsx
+++ App.tsx
@@ -7,9 +7,35 @@
 
 import React from 'react';
 import {View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Chat from './screens/Chat';
+
+const Stack = createStackNavigator();
+
+// CHAT STACK NAVIGATOR
+
+function ChatStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Chat" component={Chat} />
+    </Stack.Navigator>
+  );
+}
+
+// ROOT STACK NAVIGATOR
+
+function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <ChatStack />
+    </NavigationContainer>
+  );
+}
 
 function App() {
-  return <View></View>;
+  return <RootNavigator />;
 }
 
 export default App;
--- screens/Chat.js
+++ screens/Chat.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {GiftedChat} from 'react-native-gifted-chat';
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    // Esegui solo al mount
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
+  }, []); // Array di dipendenze vuoto per eseguire solo al mount
+
+  const onSend = (newMessages = []) => {
+    setMessages(prevMessages => GiftedChat.append(prevMessages, newMessages));
+  };
+
   return (
-    <>
-      <GiftedChat />
-    </>
+    <GiftedChat
+      messages={messages}
+      onSend={newMessages => onSend(newMessages)}
+      user={{
+        _id: 1,
+      }}
+    />
   );
 };
 
