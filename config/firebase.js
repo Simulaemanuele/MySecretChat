946a7156e2a5ca15c993ded34b46200b68784691
Emanuele Simula <simulaemanuele@gmail.com>
2023-11-29 10:30:37 +0100

    feat: setted env variables in .env file and configured Firebase in a folder config with firebase.js file. Updated settings in android/app/build.gradle file.

---
+++ config/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+import Config from 'react-native-config';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: Config.API_KEY,
+  authDomain: Config.AUTH_DOMAIN,
+  projectId: Config.PROJECT_ID,
+  storageBucket: Config.STORAGE_BUCKET,
+  messagingSenderId: Config.MESSAGING_SENDER_ID,
+  appId: Config.APP_ID,
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const database = getFirestore();
