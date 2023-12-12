0f95cbafd91dc9ba215bc0d82d44ce8d9d2678ad
Emanuele Simula <simulaemanuele@gmail.com>
2023-12-12 14:21:24 +0100

    fix+feat: implemented avatr view siding messages and isTyping feature

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,11 +1,15 @@
 import React, {useEffect, useState, useLayoutEffect, useCallback} from 'react';
-import {TouchableOpacity, Text} from 'react-native';
+import {TouchableOpacity} from 'react-native';
 import {
   collection,
   orderBy,
   addDoc,
   query,
   onSnapshot,
+  updateDoc,
+  doc,
+  getDoc,
+  setDoc,
 } from 'firebase/firestore';
 import {signOut} from 'firebase/auth';
 import {auth, database} from '../config/firebase';
@@ -16,32 +20,9 @@ import {GiftedChat} from 'react-native-gifted-chat';
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
 
   const navigation = useNavigation();
-
-  // useEffect(() => {
-  //   // Esegui solo al mount
-  //   setMessages([
-  //     {
-  //       _id: 1,
-  //       text: 'Hello developer',
-  //       createdAt: new Date(),
-  //       user: {
-  //         _id: 2,
-  //         name: 'React Native',
-  //         avatar: 'https://placeimg.com/140/140/any',
-  //       },
-  //     },
-  //   ]);
-  // }, []); // Array di dipendenze vuoto per eseguire solo al mount
-
-  // const onSend = (newMessages = []) => {
-  //   setMessages(prevMessages => GiftedChat.append(prevMessages, newMessages));
-  // };
-
-  const onSignOut = () => {
-    signOut(auth).catch(err => console.log(err));
-  };
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -56,13 +37,35 @@ const Chat = () => {
         </TouchableOpacity>
       ),
     });
-  }, [navigation]);
+
+    // Verifica se il documento utente esiste al mount del componente
+    const initializeUserDoc = async () => {
+      const currentUserEmail = auth?.currentUser?.email;
+      const userDocRef = doc(database, 'users', currentUserEmail);
+
+      try {
+        // snapshot of the user doc
+        const userDocSnapshot = await getDoc(userDocRef);
+
+        if (!userDocSnapshot.exists()) {
+          // If not exists create it
+          await setDoc(userDocRef, {
+            isTyping: false,
+          });
+        }
+      } catch (error) {
+        console.error('Error User Doc verify: ', error);
+      }
+    };
+
+    initializeUserDoc();
+  }, []);
 
   useLayoutEffect(() => {
     const collectionRef = collection(database, 'chats');
     const queryRef = query(collectionRef, orderBy('createdAt', 'desc'));
 
-    const unsuscribe = onSnapshot(queryRef, snapshot => {
+    const unsubscribe = onSnapshot(queryRef, snapshot => {
       console.log('snapshot');
       setMessages(
         snapshot.docs.map(doc => ({
@@ -74,32 +77,66 @@ const Chat = () => {
       );
     });
 
-    return () => unsuscribe();
+    return () => unsubscribe();
   }, []);
 
-  const onSend = useCallback((messages = []) => {
-    setMessages(prevMessages => GiftedChat.append(prevMessages, messages));
+  const onSend = useCallback(
+    async (messages = []) => {
+      setIsTyping(false);
+      setMessages(prevMessages => GiftedChat.append(prevMessages, messages));
 
-    const {_id, createdAt, text, user} = messages[0];
-    addDoc(collection(database, 'chats'), {
-      _id,
-      createdAt,
-      text,
-      user,
-    });
-  }, []);
+      const {_id, createdAt, text, user} = messages[0];
+      await addDoc(collection(database, 'chats'), {
+        _id,
+        createdAt,
+        text,
+        user,
+      });
+    },
+    [setMessages],
+  );
+
+  const handleInputTextChanged = async text => {
+    setIsTyping(text.length > 0);
+
+    const currentUserEmail = auth?.currentUser?.email;
+    const userDocRef = doc(database, 'users', currentUserEmail);
+
+    try {
+      // Verify is the doc exists before the update
+      const userDocSnapshot = await getDoc(userDocRef);
+
+      if (userDocSnapshot.exists()) {
+        // If exists, update isTyping field
+        await updateDoc(userDocRef, {
+          isTyping: text.length > 0,
+        });
+      } else {
+        console.error('User Doc not found.');
+      }
+    } catch (error) {
+      console.error('Error updating stat of isTyping: ', error);
+    }
+  };
+
+  const onSignOut = () => {
+    signOut(auth).catch(err => console.log(err));
+  };
 
   return (
     <GiftedChat
       messages={messages}
-      onSend={messages => onSend(messages)}
+      onSend={newMessages => onSend(newMessages)}
       user={{
         _id: auth?.currentUser?.email,
-        avatar: 'https://placeimg.com/140/140/any',
+        avatar: 'https://picsum.photos/200/300',
       }}
       messagesContainerStyle={{
         backgroundColor: colors.mediumGray,
       }}
+      isTyping={isTyping}
+      onInputTextChanged={text => handleInputTextChanged(text)}
+      showUserAvatar={true}
     />
   );
 };
